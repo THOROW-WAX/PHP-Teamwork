2c0d84fc5965a3bec708661344acb0e8bbc66eae
plami <plamena.y.nikolova@gmail.com>
2014-08-23 21:28:03 +0300

    user's ranking-html$css

---
+++ categories&users/ranking.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ranking</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css"/>
+</head>
+
+<body>
+<div>
+    <h2>Потребители</h2>
+    <table>
+        <thead>
+        <tr class="header">
+           <th>№</th>
+           <th>Потребител</th>
+           <th class="header_points">Точки</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td class="ranking">1</td>
+            <td class="profile">
+              <img src="img/user1.jpg" width="60">
+              <label>бай Васо</label>
+              <span class="label" style="color: #ffffff;background:#6db4f1">Admin</span>
+            </td>
+            <td class="points">1950</td>
+        </tr>
+        <tr>
+        <td class="ranking">2</td>
+        <td class="profile">
+            <img src="img/user2.jpg" width="60">
+            <label>бай Венци</label>
+            <span class="label" style="color: #ffffff;background:#6db4f1">Admin</span>
+        </td>
+        <td class="points">1500</td>
+        </tr>
+        <tr>
+            <td class="ranking">3</td>
+            <td class="profile">
+                <img src="img/user3.jpg" width="60">
+                <label>леля Гинче</label>
+                <span class="label" style="color: #ffffff;background:#FA9248">Trainer</span>
+            </td>
+            <td class="points">1245</td>
+        </tr>
+        <tr>
+            <td class="ranking">4</td>
+            <td class="profile">
+                <img src="img/user4.jpg" width="60">
+                <label>бай Нако</label>
+                <span class="label" style="color: #ffffff;background:#6db4f1">Admin</span>
+                <span class="label" style="color: #ffffff;background:#FA9248">Trainer</span>
+            </td>
+            <td class="points">1067</td>
+        </tr>
+        <tr>
+            <td class="ranking">5</td>
+            <td class="profile">
+                <img src="img/user5.jpg" width="60">
+                <label>бай Махмуд</label>
+                <span class="label" style="color: #ffffff;background:#FA9248">Trainer</span>
+            </td>
+            <td class="points">995</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
