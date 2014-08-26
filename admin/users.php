0fed2c453fbbfdb03edc2c3ac5e1466c144aac53
Kiril Georgiev <kiril.g.georgiev90@gmail.com>
2014-08-26 11:08:33 +0000

    fixed minor bugs and included default time-zone

--- admin/users.php
+++ admin/users.php
@@ -4,9 +4,8 @@ session_start();
 include 'admin_header_footer.php';
 admin_header('Editing users'); 
 db_init();
-
+date_default_timezone_set("Europe/Sofia");
 echo "<h1>EDIT USERS & CHANGE PERMISSIONS</h1>";
-
 switch ((isset($_POST['submit'])== 1 ? $_POST['submit'] : '')) {
 	case 'Create': users_add(); break;
 	case 'Submit': users_edit(); break;
@@ -15,7 +14,7 @@ switch ((isset($_POST['submit'])== 1 ? $_POST['submit'] : '')) {
 	default: break;
 }
 
-echo "Enter user login to edit or slecet from <a href='../users.php'>users page</a> ";
+echo "Slecet from <a href='../users.php?page=1'>users page</a> to edit ";
 
 if (isset($_GET['mode']) == "edit" && (isset($_GET['id']) > 0)) {
     $id = (int)$_GET['id'];
@@ -30,7 +29,7 @@ if (isset($_GET['mode']) == "edit" && (isset($_GET['id']) > 0)) {
     User Password: <input type="text" name="pass" value="<?php  echo isset($user_info['pass']) == 1?$user_info['pass']:''  ?>" /><br />
     Name: <input type="text" name="name" value="<?php  echo isset($user_info['real_name']) == 1?$user_info['real_name']:''  ?>" /><br />
     Email: <input type="email" name="email" value="<?php  echo isset($user_info['email']) == 1?$user_info['email']:''  ?>" /><br />
-    Date registered: <?php  echo isset($user_info['date_reg']) == 1?$user_info['date_reg']:''  ?></br>
+    Date registered: <?php  echo isset($user_info['date_reg']) == 1?date("d-m-Y",$user_info['date_reg']):''  ?></br>
     Status:<select name="status">
                 <option <?php  echo isset($user_info['status']) == 1? $user_info['status'] == 1 ? 'selected="selected"':'' :''  ?> value="1" >User</option>
                 <option <?php  echo isset($user_info['status']) == 1? $user_info['status'] == 2 ? 'selected="selected"':'' :''  ?> value="2" >Admin</option>
