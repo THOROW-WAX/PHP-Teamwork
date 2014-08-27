548b3ac3351f365d64d0d80c492bad83adc4aa09
plami <plamena.y.nikolova@gmail.com>
2014-08-27 08:54:29 +0300

    added about.php

---
+++ design_forum/about.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About</title>
+    <link rel="stylesheet" href="css/forum.css" type="text/css"/>
+</head>
+
+<body>
+<header>
+    <div>
+        <img src="img/header.jpg" />
+        <h1>The Forum of "Fakulteto-Uni"</h1>
+        <nav>
+            <a href="register.php">Register</a>
+            <a href="login.php">Login</a>
+            <a href="users.php">Users</a>
+            <a href="about.php">About</a>
+        </nav>
+    </div>
+</header>
+<section>
+    <h2>TEAM "THOROW-WAX":</h2>
+    1.Kristian Mariyanov (kristian.mariyanov@gmail.com)<br/>
+    <img src="img/kiko.jpg" width="70"/><br />
+    2.Kiril Georgiev (kiril.g.georgiev90@gmail.com) <br />
+    <img src="img/kiro.jpg" width="70"/><br />
+    3.Vassil Nikov (...) <br />
+    <img src="img/vasko.jpg" width="70"/><br />
+    4.Plamena Nikolova (plamena.y.nikolova@gmail.com) <br />
+    <img src="img/plami.jpg" width="70"/><br />
+    5.Ivaylo Iiev (ivaylo.il@gmail.com) <br />
+    <img src="img/ivaylo.jpg" width="70"/><br />
+</section>
+
+<footer>
+    <p>© 2014 by Team "THOROW-WAX". All Rights Reserved.</p>
+</footer>
+</body>
+</html>
